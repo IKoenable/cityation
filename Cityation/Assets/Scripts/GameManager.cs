4268346778daf1b7f38f70f2f3c895de3e5dd0a2
Vire89 <erik_verhaar@hotmail.com>
2020-10-21 20:18:36 +0200

    Auto's om de beurt besturen - versie 1

--- Cityation/Assets/Scripts/GameManager.cs
+++ Cityation/Assets/Scripts/GameManager.cs
@@ -5,24 +5,92 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     public InputController inputController;
     private CarController[] _vehicles; // Array of vehicles
+    private ReplayManager[] _replayManagers;
+    public int selectedCar = 0;
+    private int _numberOfCars = 0;
+    private CameraFollow _cameraFollow;
+    private Vector3[] _initialPosition;
+    private Transform[] _transforms;
 
     void Start()
     {
         _vehicles = FindObjectsOfType<CarController>();
+        _numberOfCars = _vehicles.Length;
+        _replayManagers = new ReplayManager[_numberOfCars];
+        _initialPosition = new Vector3[_numberOfCars];
+        _transforms = new Transform[_numberOfCars];
+
+        for (int i = 0; i < _numberOfCars; i++)
+        {
+            _replayManagers[i] = _vehicles[i].gameObject.GetComponent<ReplayManager>();
+            _transforms[i] = _vehicles[i].gameObject.GetComponent<Transform>();
+            _vehicles[i].isControlled = (i == selectedCar);
+            _initialPosition[i]= _vehicles[i].gameObject.transform.position;
+        }
+
+        _cameraFollow = FindObjectOfType<CameraFollow>();
+        SelectCar(selectedCar);
+
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         foreach (var vehicle in _vehicles)
         {
             if (vehicle.isControlled)
-            {
-                vehicle.Steer = inputController.SteerInput;
-                vehicle.Throttle = inputController.ThrottleInput;
-                vehicle.IsBraking = inputController.IsBraking;
-            }
+            { SteerFromInput(vehicle); }
+            else
+            { SteerFromAI(vehicle); }
+        }
+        
+        if (_vehicles[selectedCar].gameObject.transform.position.z > 200)
+        {
+            Debug.Log("trigger");
+            SelectCar((selectedCar + 1) % _numberOfCars);
+        }
+    }
+
+    private void SteerFromInput(CarController vehicle)
+    {
+        vehicle.Steer = inputController.SteerInput;
+        vehicle.Throttle = inputController.ThrottleInput;
+        vehicle.IsBraking = inputController.IsBraking;
+    }
+
+    private void SteerFromAI(CarController vehicle)
+    {
+        vehicle.Steer = 0;
+        vehicle.Throttle = 0;
+        vehicle.IsBraking = false;
+    }
+
+    private void SelectCar(int iSelect)
+    {
+        if (_replayManagers[selectedCar].IsRecording)
+        {
+            _replayManagers[selectedCar].StopRecording();
+
         }
 
+        _vehicles[selectedCar].isControlled = false;
+        selectedCar = iSelect;
+        _vehicles[selectedCar].isControlled = true;
+
+        _transforms[selectedCar].position = _initialPosition[selectedCar];
+
+        for (int i = 0; i < _numberOfCars; i++)
+        {
+            if (i==selectedCar)
+            {
+                _replayManagers[i].StartRecording();
+            }
+            else
+            {
+                _replayManagers[i].StartReplaying();
+            }
+        }
+        _cameraFollow.target = _vehicles[selectedCar].gameObject.transform;
     }
+    
 }
---
+++ Cityation/Assets/Scripts/ReplayManager.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+
+public class ReplayManager : MonoBehaviour
+{
+    private Transform[] transforms;
+
+    private MemoryStream memoryStream = null;
+    private BinaryWriter binaryWriter = null;
+    private BinaryReader binaryReader = null;
+    private bool recordingInitialized=false;
+    public bool IsRecording; // { get; private set; }
+    public bool IsReplaying { get; private set; }
+
+
+
+    public void StartRecording()
+    {
+        if (!recordingInitialized)
+        {
+            InitializeRecording();
+        }
+        else
+        {
+            memoryStream.SetLength(0);
+        }
+        ResetReplayFrame();
+        IsRecording = true;
+    }
+
+    public void StartReplaying()
+    {
+        if (IsRecording)
+        {
+            StopRecording();
+        }
+        if (recordingInitialized)
+        {
+            ResetReplayFrame();
+            IsReplaying = true;
+        }
+
+    }
+
+    public void StopRecording()
+    {
+        IsRecording = false;
+    }
+
+    public void StopReplaying()
+    {
+        IsReplaying = false;
+    }
+
+
+    void Start()
+    {
+        //transforms = GetComponents<Transform>();
+        transforms = GetComponents<Transform>();
+        // transforms = transforms.Concat(<Transform>()).ToArray;
+        IsRecording = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (IsRecording)
+        {
+            UpdateRecording();
+        }
+        else if (IsReplaying)
+        {
+            UpdateReplaying();
+        }
+
+    }
+
+    private void UpdateRecording()
+    {
+        SaveTransforms(transforms);
+    }
+
+    private void UpdateReplaying()
+    {
+        if (memoryStream.Position >= memoryStream.Length)
+        {
+            StopReplaying();
+            return;
+        }
+        LoadTransforms(transforms);
+    }
+
+    private void InitializeRecording()
+    {
+        memoryStream = new MemoryStream();
+        binaryWriter = new BinaryWriter(memoryStream);
+        binaryReader = new BinaryReader(memoryStream);
+        recordingInitialized = true;
+    }
+
+
+
+
+
+    private void ResetReplayFrame()
+    {
+        memoryStream.Seek(0, SeekOrigin.Begin);
+        binaryWriter.Seek(0, SeekOrigin.Begin);
+    }
+
+    private void SaveTransforms(Transform[] transforms)
+    {
+        foreach (Transform transform in transforms)
+        {
+            SaveTransform(transform);
+        }
+    }
+    private void SaveTransform(Transform transform)
+    {
+        binaryWriter.Write(transform.position.x);
+        binaryWriter.Write(transform.position.y);
+        binaryWriter.Write(transform.position.z);
+        binaryWriter.Write(transform.eulerAngles.x);
+        binaryWriter.Write(transform.eulerAngles.y);
+        binaryWriter.Write(transform.eulerAngles.z);
+    }
+
+    private void LoadTransforms(Transform[] transforms)
+    {
+        foreach (Transform transform in transforms)
+        {
+            LoadTransform(transform);
+        }
+    }
+
+    private void LoadTransform(Transform transform)
+    {
+        float x= binaryReader.ReadSingle();
+        float y = binaryReader.ReadSingle();
+        float z = binaryReader.ReadSingle();
+        transform.position = new Vector3(x, y, z);
+        float xRot = binaryReader.ReadSingle();
+        float yRot = binaryReader.ReadSingle();
+        float zRot = binaryReader.ReadSingle();
+        transform.eulerAngles = new Vector3(xRot, yRot, zRot);
+    }
+
+
+}
